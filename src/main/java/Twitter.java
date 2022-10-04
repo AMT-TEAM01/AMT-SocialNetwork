3426ae84b1cb6eb148a8ae2ed3f022723f879350
Janis Chiffelle <janischiffelle@hotmail.ch>
2022-10-04 14:25:30 +0200

    refractor twitter constructor

--- src/main/java/Twitter.java
+++ src/main/java/Twitter.java
@@ -1,52 +1,110 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Twitter implements IObservable {
 
-    //region private attributes
-    private List<IObserver> _observers = new ArrayList<IObserver>();
-    private List<String> _twits = new ArrayList<String>();
-    //endregion private attributes
+    private List<IObserver> _observers = new LinkedList<IObserver>();
+    private final LinkedList<String> _twits = new LinkedList<String>();
 
-    public Twitter(){}
+    public Twitter() { }
 
-    public Twitter(List<IObserver> observers){
-        throw new UnsupportedOperationException();
+    public Twitter(List<IObserver> observers) {
+        subscribe(observers);
     }
 
     public List<IObserver> getObservers(){
-        throw new UnsupportedOperationException();
+        return _observers;
     }
+
 
     public List<String> getTwits(){
-        throw new UnsupportedOperationException();
+        return _twits;
     }
 
-    public String lastTwit(){
-        throw new UnsupportedOperationException();
-    }
-
-    public void post(String twit){
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void subscribe(List<IObserver> observer) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void unsubscribe(IObserver observer) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
+    /// <summary>
+    /// This method is designed to trigger update to each subscriber
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
     public void notifyObservers() {
+        if(_observers.isEmpty()){
+            throw new EmptyListOfSubscribersException();
+        }
+        for(var observer : _observers){
+            observer.update(this);
+        }
+    }
+
+    /// <summary>
+    /// This method is designed to add a subscriber in the list of observers
+    /// </summary>
+    /// <param name="observers">Observers to add to the list of observers</param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void subscribe(List<IObserver> observers) {
+        for(var obs : observers){
+            if(_observers.contains(obs)){
+                throw new SubscriberAlreadyExistsException();
+            }
+            _observers.add(obs);
+        }
+    }
+
+    /// <summary>
+    /// This method is designed to remove a subscriber from the existing list of observers
+    /// </summary>
+    /// <param name="observer">Observer to remove from the list of observers</param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void unsubscribe(IObserver observer) {
+        if(_observers.isEmpty()){
+            throw new EmptyListOfSubscribersException();
+        }
+        if(!_observers.contains(observer)){
+            throw new SubscriberNotFoundException();
+        }
+        _observers.remove(observer);
+    }
+
+    /// <summary>
+    /// This method is designed to post a twit
+    /// </summary>
+    /// <param name="twit">Twit to post</param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void post(String twit) {
         throw new UnsupportedOperationException();
     }
 
-    public class TwitterException extends Exception { }
-    public class EmptyListOfSubscribersException extends TwitterException { }
-    public class SubscriberAlreadyExistsException extends TwitterException { }
-    public class SubscriberNotFoundException extends TwitterException { }
+    public String lastTwit() {
+        throw new UnsupportedOperationException();
+    }
+
+
+    private Boolean exists(IObserver followerToFind) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    public class TwitterException extends RuntimeException {
+        public TwitterException(){
+            super();
+        }
+    }
+
+    public class EmptyListOfSubscribersException extends TwitterException {
+        public EmptyListOfSubscribersException() {
+            super();
+        }
+    }
+
+    public class SubscriberAlreadyExistsException extends TwitterException {
+        public SubscriberAlreadyExistsException() {
+            super();
+        }
+    }
+
+    public class SubscriberNotFoundException extends TwitterException {
+
+        public SubscriberNotFoundException() {
+            super();
+        }
+    }
+
 }
